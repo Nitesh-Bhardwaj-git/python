8f469bb59e4039ac9e0836827d168c99ecd4db72
Nitesh Kumar <114833656+Nitesh-Bhardwaj-git@users.noreply.github.com>
2024-01-25 19:26:21 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,10 @@
+def calculate_sum(numbers):
+   
+    sum_of_numbers = sum(numbers)
+    return sum_of_numbers
+
+numbers_list = [1, 2, 3, 4, 5]
+result = calculate_sum(numbers_list)
+print(f"The sum of numbers {numbers_list} is: {result}")
+
+
